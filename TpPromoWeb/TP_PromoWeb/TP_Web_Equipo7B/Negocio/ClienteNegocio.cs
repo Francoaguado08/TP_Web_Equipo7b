cc956902754fae7a1ae7627780aa8c021ccb5c99
Francoaguado08 <aguadoezequiel08@gmail.com>
2024-10-05 00:22:33 -0300

    Agrego ClienteNegocio con metodo (obtener por DNI) Y (Registar)

---
+++ TpPromoWeb/TP_PromoWeb/TP_Web_Equipo7B/Negocio/ClienteNegocio.cs
@@ -0,0 +1,85 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ClienteNegocio
+    {
+        public Cliente ObtenerPorDNI(string dni)
+        {
+            Cliente cliente = null;
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT * FROM Clientes WHERE Documento = @Documento");
+                datos.setearParametro("@Documento", dni);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    cliente = new Cliente
+                    {
+                        Id = (int)datos.Lector["Id"],
+                        Documento = (string)datos.Lector["Documento"],
+                        Nombre = (string)datos.Lector["Nombre"],
+                        Apellido = (string)datos.Lector["Apellido"],
+                        Email = (string)datos.Lector["Email"],
+                        Direccion = (string)datos.Lector["Direccion"],
+                        Ciudad = (string)datos.Lector["Ciudad"],
+                        CP = (int)datos.Lector["CP"]
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            return cliente;
+        }
+
+        public void Registrar(Cliente cliente)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO Clientes (Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP) " +
+                                     "VALUES (@Documento, @Nombre, @Apellido, @Email, @Direccion, @Ciudad, @CP)");
+                datos.setearParametro("@Documento", cliente.Documento);
+                datos.setearParametro("@Nombre", cliente.Nombre);
+                datos.setearParametro("@Apellido", cliente.Apellido);
+                datos.setearParametro("@Email", cliente.Email);
+                datos.setearParametro("@Direccion", cliente.Direccion);
+                datos.setearParametro("@Ciudad", cliente.Ciudad);
+                datos.setearParametro("@CP", cliente.CP);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
