f9868a583a5f0abadb4a5fe86de400d33005ce3d
Francoaguado08 <aguadoezequiel08@gmail.com>
2024-10-05 00:16:53 -0300

    Agrego clase Voucher y Cliente

---
+++ TpPromoWeb/TP_PromoWeb/TP_Web_Equipo7B/Dominio/Voucher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Voucher
+    {
+
+        public string CodigoVoucher { get; set; }
+        public Cliente Cliente { get; set; } // Relación con el cliente
+        public DateTime FechaCanje { get; set; } // Fecha cuando el voucher fue canjeado
+        public Articulo Articulo { get; set; } // Premio seleccionado
+
+
+    }
+}
